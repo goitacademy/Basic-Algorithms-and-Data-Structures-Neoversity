d55b0ea1b43f36addb2c813af9dd9e0bd4f33b14
Oleh Osadchuk <olegosa1601@gmail.com>
2024-10-03 20:22:03 +0300

    Update tested selection sort with python build in functions

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -12,6 +12,16 @@ array = [12, 11, 13, 5, 6, 7]
 print(f"Given array is {array}")  # Виводимо початковий масив
 print(f"Sorted array is {selection_sort(array)}")  # Виводимо відсортований масив
 
+def test_selection(arr: list):
+    for i in range(len(arr)):
+        min_index = arr.index(min(arr[i:]), i)
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+array = [12, 11, 13, 5, 6, 7]
+print(f"Test Given array is {array}")  # Виводимо початковий масив
+print(f"Test Sorted array is {test_selection(array)}")  # Виводимо відсортований масив
+
 
 
 def selection_sort_recursive(arr, index=0):
